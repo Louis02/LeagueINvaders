887e5ef49485641ad2c3c7d5b48c3efe2f081e8c
League <league@WTS8.attlocal.net>
2018-02-28 16:18:14 -0800

    finished yayayayayayayaay

--- src/Projectile.java
+++ src/Projectile.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Projectile extends GameObject {
-	int speed = 10;
+	int speed = 7;
 
 	public Projectile(int x, int y, int width, int height) {
 		super(x, y, width, height);
@@ -18,7 +18,8 @@ public class Projectile extends GameObject {
 	}
 
 	public void draw(Graphics g) {
-		g.setColor(Color.red);
-		g.fillRect(x, y, width, height);
+        g.drawImage(GamePanel.bulletImg, x, y, width, height, null);
+
+
 	}
 }
